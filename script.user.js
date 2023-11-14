fa8647ed2894d5d66c52fe9aa2d712c4e78ec6f6
madkarmaa <madkarma99@gmail.com>
2023-11-14 10:07:00 +0100

    feat: script urls

--- script.user.js
+++ script.user.js
@@ -2,10 +2,10 @@
 // @name            YouTube video downloader
 // @icon            https://www.youtube.com/s/desktop/6ee70b2c/img/favicon_144x144.png
 // @namespace       aGkgdGhlcmUgOik=
-// @source
-// @supportURL
-// @updateURL
-// @downloadURL
+// @source          https://github.com/madkarmaa/youtube-downloader
+// @supportURL      https://github.com/madkarmaa/youtube-downloader
+// @updateURL       https://raw.githubusercontent.com/madkarmaa/youtube-downloader/main/script.user.js
+// @downloadURL     https://raw.githubusercontent.com/madkarmaa/youtube-downloader/main/script.user.js
 // @version         1.0.0
 // @description     Download YouTube videos locally at the best quality!
 // @author          mk_
